d1aab9c55529e422ae377e0995e61184ca72700e
INOPIAE <m.maengel@inopiae.de>
2025-04-22 06:51:16 +0200

    add: translation

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
 import Taskpane from './taskpane.vue';
+import { i18n } from '../i18n/i18n'
 
-createApp(Taskpane).mount('#app');
+createApp(Taskpane)
+    .use(i18n)
+    .mount('#app');
